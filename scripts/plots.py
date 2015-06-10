57ca73725df89ef754c33ebab6c47bc06b2f256b
Adam J. Jackson <a.j.jackson@bath.ac.uk>
2015-06-10 16:19:51 +0100

    mu contributions plots, including reduced-range variant

--- scripts/plots.py
+++ scripts/plots.py
@@ -2,6 +2,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.ticker import MultipleLocator
+from matplotlib import gridspec
 
 from matplotlib import rc, rcParams
 rc('font',**{'family':'serif', 'weight':'normal'})
@@ -73,7 +74,6 @@ def plot_composition(T, P, data, functionals=data_sets.keys(), filename=False):
         filename: path to output file. If False (default), print to screen instead.
 
     """
-    from matplotlib import gridspec
 
     fig = plt.figure(figsize =  (17.2 / 2.54, 17 / 2.54))
     gs = gridspec.GridSpec(len(functionals), len(P), bottom=0.25)
@@ -263,6 +263,100 @@ def plot_mu_functionals(data, T, P, functionals=False, filename=False, compact=F
             plt.show()
         plt.close()
 
+def plot_mu_contributions( T, P, data, functionals, T_range=(200,1500), filename=False, figsize=(17.2 / 2.54, 17 / 2.54), bottom=0.4, T_units='K', T_increment=400, mu_range=False):
+    """
+    Plot free energy of mixture, showing contributions of components.
+
+    Arguments:
+        data: dict containing Calc_n_mu namedtuples, with keys corresponding to 'functionals'.
+              Each namedtuple contains the nested lists n[P][T], mu[P][T] and list labels.
+              n: atom frac of S of each species, corresponding to labels
+              mu: free energy of mixture on atom basis in J mol-1
+              labels: identity labels of each species in mixture
+        T: Iterable of temperatures in K, corresponding to T ranges in data
+        P: Iterable of P values in Pa corresponding to data. Used for labelling: all pressures will be plotted
+        functionals: iterable containing keys of data to use. If False, all functionals
+              in 'data_sets' will be plotted.
+        T_range: 2-tuple containing temperature range of plot [default value (200,1500)]
+        filename: Filename for plot output. If False (default), print to screen instead.
+        figsize: 2-tuple containing figure dimensions in inches
+        bottom: fraction of figure vertically reserved for legend.
+        T_units: Temperature unit. If set to 'C', T_range and displayed axis are in degrees C. Input data must still be in K.
+        T_increment: Float; Spacing between temperature markers on x-axis
+        mu_range: 2-tuple. If set, force displayed y-axis range in kJ/mol
+
+    """
+    
+    fig = plt.figure(figsize=figsize)
+
+    # Allow extra space for functional labels on y axes
+    if len(functionals) > 1:
+        left = 0.2
+    else:
+        left=0.15
+    gs = gridspec.GridSpec(len(functionals), len(P), left=left, bottom=bottom)
+
+    tick_length = 4
+    tick_width = 0.5
+
+    if T_units == 'C':
+        T_offset=-273.15
+        T_label = r'Temperature / $^\circ$C'
+    else:
+        T_offset=0
+        T_label = r'Temperature / K'
+    
+    for row, functional in enumerate(functionals):
+        db_file = data_directory + '/' + data_sets[functional]
+        labels, thermo, a = unpack_data(db_file, ref_energy=reference_energy(db_file, units='eV'))
+        
+        color_cycle = [species_colors[species] for species in data[functional].labels]
+        for col, p in enumerate(P):
+            if col == 0:
+                ax = plt.subplot(gs[row,col])
+                left_ax = ax
+            else:
+                ax = plt.subplot(gs[row,col], sharey=left_ax)
+               
+            ax.set_color_cycle(color_cycle)
+            ax.plot([t + T_offset for t in T], [get_potentials(thermo,T=t,P_ref=p) * 1E-3 / a for t in T])
+            
+            ax.plot([t + T_offset for t in T], [mu * 1E-3 for mu in data[functional].mu[col]], 'm:', linewidth=4)
+            ax.tick_params('both',length=tick_length,width=tick_width, which='both')
+
+            ml = MultipleLocator(T_increment)
+            ax.xaxis.set_major_locator(ml)
+            ax.axes.set_xlim(T_range)
+            if mu_range:
+                ax.axes.set_ylim(mu_range)
+
+            if row == 0:
+                ax.set_title("$10^{" + "{0:d}".format(int(np.log10(p))) + "}$ Pa", fontweight='normal')
+            if row != len(functionals) -1:
+                ax.set_xticklabels('',visible=False)
+            if row == len(functionals) -1:
+                ax.axes.set_xlabel(T_label)
+                
+            # Only specify name of functional if more than one is used
+            if col == 0:
+                if len(functionals) == 1:
+                    functional_label =  r'$\mu$ / kJ mol$^{-1}$'
+                elif functional in functional_names:
+                    functional_label = functional_names[functional] + '\n' + r'$\mu$ / kJ mol$^{-1}$'
+                else:
+                    functional_label = functional + '\n' + r'$\mu$ / kJ mol$^{-1}$'
+                ax.axes.set_ylabel(functional_label)
+            else:
+                plt.setp(ax.get_yticklabels(), visible=False) # I don't understand why ax.set_yticklabels doesn't work here, but it wipes out the first column too.
+                
+    plt.legend([plt.Line2D((0,1),(0,0), color=species_colors[species]) for species in ordered_species] + [plt.Line2D((0,1),(0,0), color='m', linestyle=':', linewidth=4)],
+               [species_names[species] for species in ordered_species] + ['Mixture'], ncol=4, loc='center', bbox_to_anchor=(0.5,bottom/3.), bbox_transform=fig.transFigure, fontsize=11)
+    if filename:
+        plt.savefig(filename)
+    else:
+        plt.show()
+    
+
 def tabulate_data(data,T,P,path=''):
     """
     Write tables of composition and free energy
@@ -305,17 +399,18 @@ def main():
     # data = compute_data(T=T, functionals=['PBE0_scaled'])
     # plot_T_composition(T, data['PBE0_scaled'].n[0], data['PBE0_scaled'].labels, 'PBE0, P = 1E5' , filename=False)
 
-    T = np.arange(50,1500,10)
+    T = np.arange(50,1500,100)
     P = [10**x for x in (1,5,7)]
     data = compute_data(T=T, P=P, functionals = data_sets.keys())
     tabulate_data(data,T,P, path=data_directory)
     
-    plot_composition(T, P, data, functionals=('LDA','PBEsol','PBE0_scaled'), filename='composition.pdf')
-    plot_composition(T, P, data, functionals=data_sets.keys(), filename='all_compositions.pdf')
+    #plot_composition(T, P, data, functionals=('LDA','PBEsol','PBE0_scaled'), filename='composition.pdf')
+    #plot_composition(T, P, data, functionals=data_sets.keys(), filename='all_compositions.pdf')
     # plot_mu_functionals(data, T, P, filename=False, compact=False)  
-
+    plot_mu_contributions(T, P, data, functionals=['PBE0_scaled'], filename='mu_contributions.pdf', figsize=(17.2/2.54, 10/2.54))
+    # "Experimentalist-friendly" plot around annealing conditions in degrees C
+    plot_mu_contributions(T,P,data,functionals=['PBE0_scaled'],filename='mu_for_annealing.pdf', figsize=(17.2/2.54, 10/2.43), T_range=(100,600), T_units='C', T_increment=100, mu_range=(-100,50))
                 
 if __name__ == '__main__':
     main()
-
 
